8715f302cf17395cf553fcf611561fc725471c7b
akshitbansal-bst <akakshitbansal@gmail.com>
2021-05-09 15:04:40 +0530

    country selection added, clear button added

--- src/components/SelectCountry.vue
+++ src/components/SelectCountry.vue
@@ -1,12 +1,33 @@
 <template>
-  <div>
-    Select Country
-  </div>
+  <select
+    @change="onSelectingCountry()"
+    v-model="selectedCountry"
+    class="form-select mt-10 border block w-full p-3 rounded"
+  >
+    <option value="0">Select Country </option>
+    <option v-for="country in countries" :value="country.ID" :key="country.ID"
+      >{{ country.Country }}
+    </option>
+  </select>
 </template>
 
 <script>
 export default {
   name: "SelectCountry",
   props: ["countries"],
+  data() {
+    return {
+      selectedCountry: 0,
+    };
+  },
+  methods: {
+    // emit is used when we want to emit changes upwards
+    onSelectingCountry() {
+      const countryDetails = this.countries.find(
+        (country) => country.ID === this.selectedCountry
+      );
+      this.$emit("get-country-data", countryDetails);
+    },
+  },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,14 @@
   <main v-if="!loading">
     <Title :title="title" :dataDate="dataDate" />
     <Body :stats="stats" />
-    <SelectCountry :countries="countries" />
+    <SelectCountry @get-country-data="onCountryChange" :countries="countries" />
+    <button
+      v-if="stats.Country"
+      @click="clearSelection"
+      class="bg-blue-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-blue-600"
+    >
+      Clear
+    </button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -42,6 +49,17 @@ export default {
       );
       const covidData = await covidApiResponse.json();
       return covidData;
+    },
+    onCountryChange(country) {
+      this.title = country.Country;
+      this.stats = country;
+    },
+    async clearSelection() {
+      this.loading = true;
+      const freshData = await this.getData();
+      this.title = "Global";
+      this.stats = freshData.Global;
+      this.loading = false;
     },
   },
   async created() {
