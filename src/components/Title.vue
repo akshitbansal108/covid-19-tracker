11d2922a8bee37088e049ea44aa62058aa7941a6
akshitbansal-bst <akakshitbansal@gmail.com>
2021-05-09 12:06:49 +0530

    header, title and global data added

---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    <div class="text-3xl font-bold">
+      {{ title }}
+    </div>
+    <div class="text-2xl mt-4 mb-10">
+      {{ formattedDate }}
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "Title",
+  props: ["title", "dataDate"],
+  computed: {
+    formattedDate: function() {
+      return moment(this.dataDate).format("MMMM Do YYYY, h:mm:ss a");
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,53 @@
 <template>
-  <div class="container">
-    Hello World
-  </div>
+  <main v-if="!loading">
+    <Title :title="title" :dataDate="dataDate" />
+    <Body :stats="stats" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching your request
+    </div>
+    <!-- : before src is used for binding loadingImage -->
+    <img :src="loadingImage" alt="loader" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
+import Title from "@/components/Title";
+import Body from "@/components/Body";
 
+export default {
+  name: "Home",
+  components: {
+    Title,
+    Body,
+  },
+  data() {
+    return {
+      loading: true,
+      dataDate: "",
+      title: "Global",
+      countries: [],
+      stats: {},
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async getData() {
+      const covidApiResponse = await fetch(
+        "https://api.covid19api.com/summary"
+      );
+      const covidData = await covidApiResponse.json();
+      return covidData;
+    },
+  },
+  async created() {
+    const covidData = await this.getData();
+
+    this.dataDate = covidData.Date;
+    this.countries = covidData.Countries;
+    this.stats = covidData.Global;
+    this.loading = false;
+  },
+};
 </script>
